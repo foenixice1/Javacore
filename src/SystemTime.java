59170d5dfe3b5e6a5684de1ebf40a19120922476
foenixice1 <foenixice1@gmail.com>
2021-06-07 16:55:01 +0700

    fullthuchanh

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -1,8 +1,115 @@
 import java.util.Date;
+import java.util.Scanner;
 
 public class SystemTime {
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now);
+//       Bai time
+//        Date now = new Date();
+//        System.out.println(now);
+//Bai in ra
+//        int i = 10 ;
+//        float f = 20.5f ;
+//        double d = 20.5d ;
+//        boolean b = true ;
+//        char c = 'a' ;
+//        String s = "Hà Nội" ;
+//        System.out.println("i = " + i);
+//        System.out.println("f = " + f);
+//        System.out.println("d = " + d);
+//        System.out.println("b = " + b);
+//        System.out.println("c = " + c);
+//        System.out.println("s = " + s);
+//toan tu
+//        float width ;
+//        float height ;
+//        Scanner scanner = new Scanner(System.in) ;
+//        System.out.println("Enter width: ");
+//        width = scanner.nextFloat();
+//        System.out.println("Enter heigth: ");
+//        height = scanner.nextFloat();
+//        float area = width * height ;
+//        System.out.println("Area is : " + area);
+//PTBN
+//        System.out.println("Linear Equation Resolver");
+//        System.out.println("Given a equation as 'a * x + b = c', please enter constants:");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("a :");
+//        double a = scanner.nextDouble();
+//        System.out.println("b :");
+//        double b = scanner.nextDouble();
+//        System.out.println("c :");
+//        double c = scanner.nextDouble();
+//
+//        if (a != 0) {
+//            double answer = ( c - b ) / a;
+//            System.out.printf("Equation pass with x = %f!\n", answer);
+//        }else  {
+//            if ( b == c ) {
+//                System.out.print("The solution is all x!");
+//            }else {
+//                System.out.print("No solution!");
+//            }
+//
+//        }
+//Tinhsongay
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhập vào tháng bạn muốn tìm :");
+//        int month = scanner.nextInt();
+//        switch (month) {
+//            case 2 :
+//                System.out.println("Tháng 2 có 28 hoặc 29 ngày");
+//                break;
+//            case 1:
+//            case 3:
+//            case 5:
+//            case 7:
+//            case 8:
+//            case 10:
+//            case 12:
+//                System.out.println("Tháng " + month + " có 31 ngày");
+//                break;
+//            case 4:
+//            case 6:
+//            case 9:
+//            case 11:
+//                System.out.println("Tháng " + month + "có 30 ngày");
+//                break;
+//        }
+//kiemtranamnhuan
+//        Scanner scanner = new Scanner(System.in);
+//        int yeah ;
+//        System.out.println("Nhập vào năm bạn muốn kiểm tra ");
+//        yeah = scanner.nextInt();
+//
+//        if ( yeah % 4 == 0 ) {
+//            if  (yeah % 100 == 0) {
+//                if ( yeah % 400 == 0) {
+//                    System.out.printf("is a leap year", yeah);
+//                }else {
+//                    System.out.printf("is NOT a leap year" , yeah);
+//                }
+//            }else {
+//                System.out.printf("is a leap yeah", yeah);
+//            }
+//        }else {
+//            System.out.printf("is Not a leap yeah" , yeah);
+//        }
+////BMI
+//        Scanner scanner = new Scanner(System.in) ;
+//        double weight , height , bmi ;
+//        System.out.println("Nhập vào số cân :");
+//        weight = scanner.nextDouble();
+//        System.out.println("Nhập vào chiều cao :");
+//        height = scanner.nextDouble();
+//        bmi = weight / Math.pow(height,2) ;
+//
+//        if ( bmi < 18)
+//            System.out.println("Bạn đang quá gầy");
+//        else if ( bmi < 25.0)
+//            System.out.println("Người bạn quá đẹp");
+//        else if ( bmi <30.0 )
+//            System.out.println("Bạn đang bị béo phì");
+//        else
+//            System.out.println("Bạn quá béo. Hãy giảm cân ngay !");
     }
 }
