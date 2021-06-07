60a3ddf35e1ee1796642da14e209e8c9a3f3d570
foenixice1 <foenixice1@gmail.com>
2021-06-07 17:13:20 +0700

    BT3

---
+++ src/BT3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BT3 {
+    public static void main(String[] args) {
+        int vnd , usd;
+        System.out.println("Nhập vào số tiền bạn cần chuyển đổi");
+        Scanner scanner = new Scanner(System.in);
+        usd = scanner.nextInt();
+        vnd = 23000 * usd ;
+        System.out.println("Số tiền quy đổi ra VND là : " + vnd + " VND");
+    }
+}
