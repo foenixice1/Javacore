67f5e158a59630a60c7c6148f99195ef5e91d20e
foenixice1 <foenixice1@gmail.com>
2021-06-07 17:07:41 +0700

    BT1

---
+++ src/BT1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BT1 {
+    public static void main(String[] args) {
+        System.out.println("Enter the name :");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello : " + name );
+    }
+}
