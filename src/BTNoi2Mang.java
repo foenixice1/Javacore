5edbcd338ef6e62adf6dcd5030284c79ff9cd6e0
foenixice1 <foenixice1@gmail.com>
2021-06-09 21:02:18 +0700

    demo

---
+++ src/BTNoi2Mang.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class BTNoi2Mang {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {4, 5, 6};
+        int length = arr1.length + arr2.length;
+        int[] result = new int[length];
+        int pos = 0;
+        for (int element : arr1) {
+            result[pos] = element;
+            pos++;
+        }
+        for (int element : arr2) {
+            result[pos] = element;
+            pos++;
+        }
+        System.out.println(Arrays.toString(result));
+    }
+}
